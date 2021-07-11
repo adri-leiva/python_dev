e8d9bc33f4d050f6fc4a871bb19ad4b392a14303
ADRIANA <you@example.com>
2021-07-11 13:25:54 -0300

    Ejercio 1: Variables e impresion por pantalla

---
+++ Ejercicios_bloque1/ejercicio1.py
@@ -0,0 +1,17 @@
+""" 
+EJERCICIO 1
+- Crear variables una "pais" y otra "continente"
+- Mostrar su valor por pantalla (imporimi)
+- Poner un comentario poniendo el tipo de dato 
+"""
+
+pais = "Argentina" #tipo de detos: cadena
+continente = "Americano" #tipo de detos: cadena
+
+""" print(pais) 
+print (continente) 
+ """
+
+#print ("{} \n {}" . format(pais,continente))
+
+print(f" Pais: {pais} \n Continente: {continente}")
