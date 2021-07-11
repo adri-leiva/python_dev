8449d1ca53e89b0a2c44541c1f5af74c3f1c14cf
ADRIANA <you@example.com>
2021-07-11 13:30:42 -0300

    Ejercio 3: Estructura FOR y WHILE

---
+++ Ejercicios_bloque1/ejercicio3.py
@@ -0,0 +1,18 @@
+""" 
+EJERCICO 3
+
+Escribir un programa que muestre los cuadrados  (el numero multiplicado por si mismo) 
+de los 60 primeros 60 numero naturales. Resolver con for y con while.
+
+"""
+
+contador = 1
+
+for contador in range (1,61):
+      print (f"{contador}^2= {contador*contador}")
+
+""" while contador<=60:
+    print (f"{contador}^2= {contador*contador}")
+    contador +=1
+"""    
+
